4d0b6b4eb88c1f92ced8cd8545e4030c870ef570
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-05-03 10:25:26 -0500

    Add item: Snowglobe

---
+++ Items/Equipment/Snowglobe.cs
@@ -0,0 +1,128 @@
+﻿using BepInEx.Configuration;
+using RoR2;
+using RoR2.Orbs;
+using System.Collections.ObjectModel;
+using UnityEngine;
+using static ThinkInvisible.ClassicItems.MiscUtil;
+using static ThinkInvisible.ClassicItems.ClassicItemsPlugin.MasterItemList;
+using UnityEngine.Networking;
+using R2API;
+using UnityEngine.Rendering.PostProcessing;
+using System.Collections.Generic;
+
+namespace ThinkInvisible.ClassicItems {
+    public class Snowglobe : ItemBoilerplate {
+        public override string itemCodeName {get;} = "Snowglobe";
+
+        private ConfigEntry<float> cfgProcRate;
+        private ConfigEntry<int> cfgDuration;
+        private ConfigEntry<float> cfgFreezeTime;
+        private ConfigEntry<float> cfgSlowTime;
+        private ConfigEntry<bool> cfgSlowUnfreezable;
+        
+        public float procRate {get;private set;}
+        public int duration {get;private set;}
+        public float freezeTime {get;private set;}
+        public float slowTime {get;private set;}
+        public bool slowUnfreezable {get;private set;}
+
+        private GameObject snowglobeControllerPrefab;
+
+        protected override void SetupConfigInner(ConfigFile cfl) {
+            cfgProcRate = cfl.Bind<float>(new ConfigDefinition("Items." + itemCodeName, "ProcRate"), 30f, new ConfigDescription(
+                "Percent chance of freezing each individual enemy for every Snowglobe tick.",
+                new AcceptableValueRange<float>(0f,100f)));
+            cfgDuration = cfl.Bind<int>(new ConfigDefinition("Items." + itemCodeName, "Duration"), 8, new ConfigDescription(
+                "Number of 1-second ticks of Snowglobe duration.",
+                new AcceptableValueRange<int>(0,int.MaxValue)));
+            cfgFreezeTime = cfl.Bind<float>(new ConfigDefinition("Items." + itemCodeName, "FreezeTime"), 1.5f, new ConfigDescription(
+                "Duration of freeze applied by Snowglobe.",
+                new AcceptableValueRange<float>(0f,float.MaxValue)));
+            cfgSlowTime = cfl.Bind<float>(new ConfigDefinition("Items." + itemCodeName, "SlowTime"), 3.0f, new ConfigDescription(
+                "Duration of slow applied by Snowglobe.",
+                new AcceptableValueRange<float>(0f,float.MaxValue)));
+            cfgSlowUnfreezable = cfl.Bind(new ConfigDefinition("Items." + itemCodeName, "SlowUnfreezable"), true, new ConfigDescription(
+                "If true, Snowglobe will slow targets even if they can't be frozen."));
+
+            procRate = cfgProcRate.Value;
+            duration = cfgDuration.Value;
+            freezeTime = cfgFreezeTime.Value;
+            slowTime = cfgSlowTime.Value;
+            slowUnfreezable = cfgSlowUnfreezable.Value;
+        }
+        
+        protected override void SetupAttributesInner() {
+            itemIsEquipment = true;
+
+            modelPathName = "snowglobecard.prefab";
+            iconPathName = "snowglobe_icon.png";
+            eqpEnigmable = true;
+            eqpCooldown = 45;
+
+            RegLang("Snowglobe",
+                "Randomly freeze enemies for 8 seconds.",
+                "Summon a snowstorm that <style=cIsUtility>freezes</style> monsters at a <style=cIsUtility>" + pct(procRate,1,1) + " chance over " + duration + " seconds</style>.",
+                "A relic of times long past (ClassicItems mod)");
+        }
+
+        protected override void SetupBehaviorInner() {
+            var ctrlPfb2 = new GameObject("snowglobeControllerPrefabPrefab");
+            ctrlPfb2.AddComponent<NetworkIdentity>();
+            ctrlPfb2.AddComponent<SnowglobeController>();
+            snowglobeControllerPrefab = ctrlPfb2.InstantiateClone("snowglobeControllerPrefab");
+            GameObject.Destroy(ctrlPfb2);
+
+            On.RoR2.EquipmentSlot.PerformEquipmentAction += On_ESPerformEquipmentAction;
+        }
+        
+        private bool On_ESPerformEquipmentAction(On.RoR2.EquipmentSlot.orig_PerformEquipmentAction orig, EquipmentSlot slot, EquipmentIndex eqpid) {
+            if(eqpid == regIndexEqp) {
+                if(!slot.characterBody || !slot.characterBody.teamComponent) return false;
+                var ctrlInst = UnityEngine.Object.Instantiate(snowglobeControllerPrefab, slot.characterBody.corePosition, Quaternion.identity);
+                ctrlInst.GetComponent<SnowglobeController>().myTeam = slot.characterBody.teamComponent.teamIndex;
+                NetworkServer.Spawn(ctrlInst);
+                return true;
+            } else return orig(slot, eqpid);
+        }
+    }
+
+	public class SnowglobeController : MonoBehaviour {
+        int remainingTicks;
+        float stopwatch = 0f;
+        public TeamIndex myTeam;
+
+        private void Awake() {
+            remainingTicks = snowglobe.duration;
+        }
+
+        private void FixedUpdate() {
+            if(!NetworkServer.active) return;
+            stopwatch += Time.fixedDeltaTime;
+            if(stopwatch >= 1f) {
+                stopwatch -= 1f;
+                remainingTicks --;
+                DoFreeze();
+                if(remainingTicks == 0) Destroy(this);
+            }
+        }
+
+        private void DoFreeze() {
+            var tind = TeamIndex.Monster | TeamIndex.Neutral | TeamIndex.Player;
+			tind &= ~myTeam;
+			ReadOnlyCollection<TeamComponent> teamMembers = TeamComponent.GetTeamMembers(tind);
+			foreach(TeamComponent tcpt in teamMembers) {
+                if(!Util.CheckRoll(snowglobe.procRate)) continue;
+                var ssoh = tcpt.gameObject.GetComponent<SetStateOnHurt>();
+                var hcpt = tcpt.gameObject.GetComponent<HealthComponent>();
+                Debug.Log(ssoh);
+                Debug.Log(hcpt);
+                if(ssoh.canBeFrozen && ssoh) {
+                    ssoh.SetFrozen(snowglobe.freezeTime);
+                }
+                if(((ssoh?.canBeFrozen ?? false) || snowglobe.slowUnfreezable) && hcpt) {
+                    hcpt.body.AddTimedBuff(BuffIndex.Slow60, snowglobe.slowTime);
+                }
+			}
+        }
+	}
+}
