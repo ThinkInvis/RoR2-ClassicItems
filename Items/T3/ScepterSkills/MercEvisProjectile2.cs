90a8a5967401e1321eb09e424467e77b22daf26c
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-05-13 19:09:48 -0500

    Scepter: Un-fix Gale-Force firing one shot at a time
    Yes, this started as a bug. Which magically fixed itself. Get back here >:(

--- Items/T3/ScepterSkills/MercEvisProjectile2.cs
+++ Items/T3/ScepterSkills/MercEvisProjectile2.cs
@@ -3,6 +3,7 @@ using RoR2.Skills;
 using static TILER2.MiscUtil;
 using R2API;
 using RoR2;
+using R2API.Utils;
 
 namespace ThinkInvisible.ClassicItems {
     public static class MercEvisProjectile2 {
@@ -15,7 +16,7 @@ namespace ThinkInvisible.ClassicItems {
             var desctoken = "CLASSICITEMS_SCEPMERC_EVISPROJDESC";
             var namestr = "Gale-Force";
             LanguageAPI.Add(nametoken, namestr);
-            LanguageAPI.Add(desctoken, Language.GetString(oldDef.skillDescriptionToken) + "\n<color=#d299ff>SCEPTER: Charges faster. Fires up to 4 projectiles at once.</color>");
+            LanguageAPI.Add(desctoken, Language.GetString(oldDef.skillDescriptionToken) + "\n<color=#d299ff>SCEPTER: Charges four times faster. Hold and fire up to four charges at once.</color>");
             
             myDef.skillName = namestr;
             myDef.skillNameToken = nametoken;
@@ -25,6 +26,25 @@ namespace ThinkInvisible.ClassicItems {
             myDef.baseRechargeInterval /= 4f;
 
             LoadoutAPI.AddSkillDef(myDef);
+        }
+
+        internal static void LoadBehavior() {
+            On.EntityStates.Commando.CommandoWeapon.FireFMJ.OnEnter += On_FireFMJEnter;
+        }
+
+        internal static void UnloadBehavior() {
+            On.EntityStates.Commando.CommandoWeapon.FireFMJ.OnEnter -= On_FireFMJEnter;
+        }
+
+        private static void On_FireFMJEnter(On.EntityStates.Commando.CommandoWeapon.FireFMJ.orig_OnEnter orig, EntityStates.Commando.CommandoWeapon.FireFMJ self) {
+            orig(self);
+            if(!(self is EntityStates.Commando.CommandoWeapon.ThrowEvisProjectile) || Scepter.instance.GetCount(self.outer.commonComponents.characterBody) < 1) return;
+            if(!self.outer.commonComponents.skillLocator?.special) return;
+            var fireCount = self.outer.commonComponents.skillLocator.special.stock;
+            self.outer.commonComponents.skillLocator.special.stock = 0;
+            for(var i = 0; i < fireCount; i++) {
+                typeof(EntityStates.Commando.CommandoWeapon.FireFMJ).GetMethodCached("Fire").Invoke(self, new object[]{});
+            }
         }
     }
 }
