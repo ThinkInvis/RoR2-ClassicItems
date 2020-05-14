01e245408b023888087bf3decfb7aa91b3f1fd00
ThinkInvisible <ThinkInvis@users.noreply.github.com>
2020-05-13 19:08:45 -0500

    Scepter: Change Rabauld to burstfire

--- Items/T3/ScepterSkills/HuntressBallista2.cs
+++ Items/T3/ScepterSkills/HuntressBallista2.cs
@@ -6,6 +6,7 @@ using EntityStates.Huntress;
 using RoR2;
 using R2API;
 using RoR2.Projectile;
+using EntityStates.Huntress.Weapon;
 
 namespace ThinkInvisible.ClassicItems {
     public static class HuntressBallista2 {
@@ -19,7 +20,7 @@ namespace ThinkInvisible.ClassicItems {
             var desctoken = "CLASSICITEMS_SCEPHUNTRESS_BALLISTADESC";
             var namestr = "Rabauld";
             LanguageAPI.Add(nametoken, namestr);
-            LanguageAPI.Add(desctoken, Language.GetString(oldDef.skillDescriptionToken) + "\n<color=#d299ff>SCEPTER: Quadruple shot count and fire rate, half damage.</color>");
+            LanguageAPI.Add(desctoken, Language.GetString(oldDef.skillDescriptionToken) + "\n<color=#d299ff>SCEPTER: Fires quick bursts of five extra projectiles for 2.5x TOTAL damage.</color>");
 
             myDef.skillName = namestr;
             myDef.skillNameToken = nametoken;
@@ -35,27 +36,37 @@ namespace ThinkInvisible.ClassicItems {
             myCtxDef.skillNameToken = nametoken;
             myCtxDef.skillDescriptionToken = desctoken;
             myCtxDef.icon = Resources.Load<Sprite>("@ClassicItems:Assets/ClassicItems/icons/scepter/huntress_ballistaicon.png");
-            myCtxDef.baseMaxStock *= 4;
-            myCtxDef.shootDelay /= 4;
 
             LoadoutAPI.AddSkillDef(myCtxDef);
         }
 
         internal static void LoadBehavior() {
             On.EntityStates.Huntress.AimArrowSnipe.OnEnter += On_AimArrowSnipeEnter;
-            On.EntityStates.Huntress.Weapon.FireArrowSnipe.ModifyBullet += On_FireArrowSnipeModify;
+            On.EntityStates.Huntress.AimArrowSnipe.OnExit += On_AimArrowSnipeExit;
+            On.EntityStates.Huntress.Weapon.FireArrowSnipe.FireBullet += On_FireArrowSnipeFire;
         }
-
-        private static void On_FireArrowSnipeModify(On.EntityStates.Huntress.Weapon.FireArrowSnipe.orig_ModifyBullet orig, EntityStates.Huntress.Weapon.FireArrowSnipe self, BulletAttack bulletAttack) {
-            orig(self, bulletAttack);
-            if(Scepter.instance.GetCount(self.outer.commonComponents.characterBody) > 0) {
-                bulletAttack.damage /= 2f;
-            }
-        }
-
         internal static void UnloadBehavior() {
             On.EntityStates.Huntress.AimArrowSnipe.OnEnter -= On_AimArrowSnipeEnter;
-            On.EntityStates.Huntress.Weapon.FireArrowSnipe.ModifyBullet -= On_FireArrowSnipeModify;
+            On.EntityStates.Huntress.AimArrowSnipe.OnExit -= On_AimArrowSnipeExit;
+            On.EntityStates.Huntress.Weapon.FireArrowSnipe.FireBullet -= On_FireArrowSnipeFire;
+        }
+
+        private static void On_FireArrowSnipeFire(On.EntityStates.Huntress.Weapon.FireArrowSnipe.orig_FireBullet orig, FireArrowSnipe self, Ray aimRay) {
+            orig(self, aimRay);
+            if(Scepter.instance.GetCount(self.outer.commonComponents.characterBody) == 0) return;
+
+            for(var i = 1; i < 6; i++) {
+                var sprRay = new Ray(aimRay.origin, aimRay.direction);
+                sprRay.direction = Util.ApplySpread(sprRay.direction, 0.1f+i/24f, 0.3f+i/8f, 1f, 1f, 0f, 0f);
+				var pew = (BulletAttack)typeof(EntityStates.GenericBulletBaseState).GetMethodCached("GenerateBulletAttack").Invoke(self, new object[]{sprRay});
+				typeof(FireArrowSnipe).GetMethodCached("ModifyBullet").Invoke(self, new object[]{pew});
+                pew.damage /= 10f/3f;
+                pew.force /= 20f/3f;
+                RoR2Application.fixedTimeTimers.CreateTimer(i*0.06f, ()=>{
+                    pew.Fire();
+			        Util.PlaySound(self.fireSoundString, self.outer.gameObject);
+                });
+            }
         }
 
         private static void On_AimArrowSnipeEnter(On.EntityStates.Huntress.AimArrowSnipe.orig_OnEnter orig, AimArrowSnipe self) {
@@ -66,6 +77,14 @@ namespace ThinkInvisible.ClassicItems {
 				sloc.primary.UnsetSkillOverride(self, AimArrowSnipe.primarySkillDef, GenericSkill.SkillOverridePriority.Contextual);
                 sloc.primary.SetSkillOverride(self, myCtxDef, GenericSkill.SkillOverridePriority.Contextual);
             }
+        }
+
+        private static void On_AimArrowSnipeExit(On.EntityStates.Huntress.AimArrowSnipe.orig_OnExit orig, AimArrowSnipe self) {
+            orig(self);
+            var sloc = self.outer.commonComponents.skillLocator;
+            if(!sloc || !sloc.primary) return;
+            if(Scepter.instance.GetCount(self.outer.commonComponents.characterBody) > 0)
+                sloc.primary.UnsetSkillOverride(self, myCtxDef, GenericSkill.SkillOverridePriority.Contextual);
         }
     }
 }
